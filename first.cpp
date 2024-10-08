d9224eed0fe8c751b494aeb4347029b621445c2c
NaikwadeAditya <naikwadeaditya7777@gmail.com>
2024-10-08 12:11:48 +0530

    Added One Addition Function

--- first.cpp
+++ first.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
-
+class Add {
+    int a;
+    int b;
+    public:
+    void Addition(int a, int b) {
+        cout << "Addition: " << a + b << endl;
+    }
+};
 int main() {
+    Add A1;
+    A1.Addition(1,1);
     cout << "This is Cpp Program.." << endl;
     cout << "Except this Pull Request";
 }
